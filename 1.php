9d2a747d4ddb0174ad9f721ec171d0b5646327c3
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-02-14 15:14:24 +0300

    подключение к бд

---
+++ 1.php
@@ -0,0 +1,21 @@
+<?php
+$host = 'my_bd';
+$username = 'root';
+$password = 'root';
+$database = 'testDB';
+
+$connect = mysqli_connect($host, $username , $password, $database);
+
+
+if($connect)
+echo "всё ок!";
+
+
+
+
+
+
+
+
+
+?>
