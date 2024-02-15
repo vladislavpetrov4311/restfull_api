1dae4809e123682dc39aaafb3e9b1745900b8703
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-02-16 00:18:38 +0300

    добавил файл .htaccess для перенаправления на /posts

---
+++ posts.php
@@ -0,0 +1,23 @@
+<?php
+
+header('Content-type: application/json');
+require 'connect.php';
+
+
+$sql = "SELECT * FROM `Q1`;";
+$posts = mysqli_query($connect , $sql);
+
+$postslist = [];
+
+while ($post = mysqli_fetch_assoc($posts)) {
+
+    $postslist[] = $post;
+
+}
+
+echo json_encode($postslist, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+
+
+
+
+?>
