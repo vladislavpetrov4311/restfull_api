a986a8a28758d5b3af2a370012366d838114242e
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-02-19 22:36:01 +0300

    добавил хедеры для настройки CORS

--- posts.php
+++ posts.php
@@ -1,5 +1,9 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: *');
+header('Access-Control-Allow-Methods: *');
+header('Access-Control-Allow-Credentials: true');
 
 header('Content-type: application/json');
 require 'connect.php';
