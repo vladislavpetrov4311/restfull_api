af46c7b784951c0feb4b29c443c1d1cafaa5fdd8
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-02-19 22:23:18 +0300

    добавил функции getpost/addpost и разметку для вывода

---
+++ 1.js
@@ -0,0 +1,41 @@
+async function getposts()
+{
+    let res = await fetch('http://localhost:9000/posts');
+    let posts = await res.json();
+
+    document.querySelector('.post-list').innerHTML = '';
+    
+   posts.forEach((post)=>{
+        document.querySelector('.post-list').innerHTML += `
+        <h5>${post.tittle}</h5>
+        <p>${post.body}</p>
+        <a href = "#">Подробнее</a>
+        `
+    })
+
+} 
+
+async function addpost()
+{
+
+    let row_id = document.getElementById("my_id").value;
+    let row_tittle = document.getElementById("my_tittle").value;
+    let row_body = document.getElementById("my_body").value;
+    
+    let formdada = new FormData();
+    formdada.append('id', row_id);
+    formdada.append('tittle' , row_tittle);
+    formdada.append('body' , row_body);
+
+    let res = await fetch('http://localhost:9000/posts' , {
+        method: 'POST',
+        body: formdada
+    });
+
+    await getposts();
+
+
+    document.getElementById("my_id").value = "";
+    document.getElementById("my_tittle").value = "";
+    document.getElementById("my_body").value = "";
+}
