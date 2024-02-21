5a7d99e968eab92ca4742bf31566ef28b22ec697
Vladislav4311 <petrov_vd_4311@mail.ru>
2024-02-22 02:56:34 +0300

    в addpost новый ID определяется самостоятельно

--- function.php
+++ function.php
@@ -34,12 +34,18 @@ function getpost($connect , $id)
 
 function addpost($connect , $data)
 {
-   
-    $id = $data['id'];
+    //получаю максимальный id на текущий момент
+    //затем инкрементирую значение на 1
+    $max_id = "SELECT MAX(`id`) AS `res` FROM `Q1`";
+    $M_id = mysqli_query($connect , $max_id);
+    $id = mysqli_fetch_assoc($M_id);
+    $new_id  = $id['res'] + 1;
+    
+
     $tittle = $data['tittle'];
     $body = $data['body'];
 
-    $sql = "INSERT INTO `Q1` (`id` , `tittle` , `body`) VALUES ($id, '$tittle', '$body');";
+    $sql = "INSERT INTO `Q1` (`id` , `tittle` , `body`) VALUES ($new_id, '$tittle', '$body');";
     $post = mysqli_query($connect , $sql);
     
 }
